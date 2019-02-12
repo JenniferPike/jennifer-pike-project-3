b171911648b4db96f99023797d2564be58bb1be1
Jennifer <jenn_e_fur16@hotmail.com>
2019-02-12 14:38:09 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//SLIDING PUZZLE
+
+// 1.CLICK ON BLACK SQUARE TO START GAME
+// 2.PHOTO TILES GET MIXED UP WITH ONE FREE SQUARE
+//      - use a random generator to move the tiles?
+// 3. CLICK ON A PHOTO TO MOVE IT TO THE OPEN SPACE
+//      - photo can move left/right and up/down
+//4. 
+//BONUS!!!!!!!!!
+//4. CELEBRATION WHEN COMPLETE
+//5. RESTART BUTTON
+//6. PICK A PHOTO - HAVE MULTIPLE PHOTO OPTIONS
+//7. COUNT MOVES
+//8. TIMER
+
+$(document).ready(function(){
+    //first I want the tile to dissappear when I click on the start
+   $('.start-square')
+
+});
