49341540b5f01ad5832dc9a54a5b7d1e22b8405c
Jennifer <jenn_e_fur16@hotmail.com>
2019-02-12 20:01:01 -0500

    working on code

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //SLIDING PUZZLE
 
 // 1.CLICK ON BLACK SQUARE TO START GAME
+//      - black square will dissappear
 // 2.PHOTO TILES GET MIXED UP WITH ONE FREE SQUARE
-//      - use a random generator to move the tiles?
+//      - use a random generator to move the tiles into different spots?
+//      - else statement so they don't end up in the same square
 // 3. CLICK ON A PHOTO TO MOVE IT TO THE OPEN SPACE
 //      - photo can move left/right and up/down
-//4. 
+//      - photos have start spots.. once all are back in those spots display complete or restart?
 //BONUS!!!!!!!!!
 //4. CELEBRATION WHEN COMPLETE
 //5. RESTART BUTTON
@@ -14,7 +16,16 @@
 //8. TIMER
 
 $(document).ready(function(){
-    //first I want the tile to dissappear when I click on the start
-   $('.start-square')
+        //this will start the game
+   $('.start-square').on('click', function(){
+        // on click remove the dark start game square
+        $('.start-square').remove('div');
+        //then it will randomize the tiles. 
+       $('.game-tile').
+   });
 
+
+   $('.game-tile').on('click', function(){
+        console.log('hello')
+   });
 });
